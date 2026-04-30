cd858d175b50c010af660b3cc99ef9a7bf81a0c9
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-04-30 22:41:35 +0600

    feat: add blog service to fetch blog posts

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  // const { data } = await userService.getSession();
-  // console.log(data);
+  const { data } = await blogService.getBlogPosts();
+  console.log(data);
 
   return (
     <div>
