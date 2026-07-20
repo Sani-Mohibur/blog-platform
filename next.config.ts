1302a691154e61a081a06d9c765786fc7caa0cd3
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-07-20 17:33:11 +0600

    chore: clean up code

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
         hostname: "example.com",
         pathname: "**",
       },
